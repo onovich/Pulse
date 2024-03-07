af22720e4f065d194d7eea413f569157227ead16
onovich <hjn1110@163.com>
2024-03-08 01:27:03 +0800

    <feature> fix: PenetrateDepthCircle_Box 未使用正确的 Sphere

--- Assets/com.mortise.pulse/Scripts_Runtime/PureFuntion/PenetratePF.cs
+++ Assets/com.mortise.pulse/Scripts_Runtime/PureFuntion/PenetratePF.cs
@@ -36,7 +36,7 @@ namespace MortiseFrame.Pulse {
         static Vector2 PenetrateDepthBox_Box(Transform aTF, BoxShape aBox, Transform bTF, BoxShape bBox) {
             // AABB & AABB
             if (aTF.RadAngle == 0 && bTF.RadAngle == 0) {
-                return PenetrateDepthAABB_AABB(aBox.GetAABB(aTF), bBox.GetAABB(aTF));
+                return PenetrateDepthAABB_AABB(aBox.GetAABB(aTF), bBox.GetAABB(bTF));
             }
             // OBB & OBB
             return PenetrateDepthOBB_OBB(aBox.GetOBB(aTF), bBox.GetOBB(bTF));
@@ -57,10 +57,10 @@ namespace MortiseFrame.Pulse {
         static Vector2 PenetrateDepthCircle_Box(Transform circleTF, CircleShape circle, Transform boxTF, BoxShape box) {
             // Circle & AABB
             if (boxTF.RadAngle == 0) {
-                return PenetrateDepthCircle_AABB(circleTF, circle, boxTF, box);
+                return PenetrateDepthAABB_Sphere(box.GetAABB(boxTF), circle.GetSphere(circleTF));
             }
             // Circle & OBB
-            return PenetrateDepthCircle_OBB(circleTF, circle, boxTF, box);
+            return PenetrateDepthOBB_Sphere(box.GetOBB(boxTF), circle.GetSphere(circleTF));
         }
 
         static Vector2 PenetrateDepthAABB_AABB(AABB a, AABB b) {
@@ -85,24 +85,22 @@ namespace MortiseFrame.Pulse {
             throw new System.Exception($"未实现 OBB x OBB 的碰撞深度计算");
         }
 
-        static Vector2 PenetrateDepthCircle_OBB(Transform circleTF, CircleShape circle, Transform aabbTF, BoxShape aabb) {
+        static Vector2 PenetrateDepthOBB_Sphere(OBB obb, Sphere sphere) {
             throw new System.Exception($"未实现 Circle x OBB 的碰撞深度计算");
         }
 
-        static Vector2 PenetrateDepthCircle_AABB(Transform circleTF, CircleShape circle, Transform aabbTF, BoxShape aabb) {
-            Vector2 aabbMin = aabbTF.Pos - aabb.Size * 0.5f;
-            Vector2 aabbMax = aabbTF.Pos + aabb.Size * 0.5f;
-
+        static Vector2 PenetrateDepthAABB_Sphere(AABB aabb, Sphere sphere) {
             Vector2 closestPoint = new Vector2(
-                Mathf.Max(aabbMin.x, Mathf.Min(circleTF.Pos.x, aabbMax.x)),
-                Mathf.Max(aabbMin.y, Mathf.Min(circleTF.Pos.y, aabbMax.y))
+                Mathf.Max(aabb.Min.x, Mathf.Min(sphere.Center.x, aabb.Max.x)),
+                Mathf.Max(aabb.Min.y, Mathf.Min(sphere.Center.y, aabb.Max.y))
             );
 
-            Vector2 toClosestPoint = closestPoint - circleTF.Pos;
-            float overlap = circle.Radius - toClosestPoint.magnitude;
+            Vector2 directionToSphere = sphere.Center - closestPoint;
+            float distanceToSphere = directionToSphere.magnitude;
+            float penetrationDepth = sphere.Radius - distanceToSphere;
 
-            if (overlap > 0) {
-                return toClosestPoint.normalized * overlap;
+            if (penetrationDepth > 0) {
+                return directionToSphere.normalized * penetrationDepth;
             } else {
                 return Vector2.zero;
             }
